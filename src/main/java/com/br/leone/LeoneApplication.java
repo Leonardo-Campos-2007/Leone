55bfcbbdf3053a6eb14663484e89ca2ffa3941a4
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-06-23 12:19:13 -0300

    New project

---
+++ src/main/java/com/br/leone/LeoneApplication.java
@@ -0,0 +1,13 @@
+package com.br.leone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeoneApplication.class, args);
+    }
+
+}
